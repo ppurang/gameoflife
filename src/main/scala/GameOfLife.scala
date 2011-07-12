53e0210a8a1255c78ce4383527ce31dde6e6f433
Piyush Purang <ppurang@gmail.com>
2011-07-12 09:09:06 +0200

    works

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -2,29 +2,27 @@ package game.of.life
 
 object `package` {
   type NeighbourhoodState  =  Seq[State]
-  val AliveNeighbours = FilterNeighbours(_ == Alive)
-  val DeadNeighbours = FilterNeighbours(_ == Dead)
   type StatePredicate = (State => Boolean)
 
-  val FilterNeighbours : StatePredicate  => (NeighbourhoodState  => Int) = (p: StatePredicate) => {
-    (x : NeighbourhoodState) => x.filter(p).size
-  }
-
+  val FilterNeighbours  = (p: StatePredicate) => (x : NeighbourhoodState) => x.filter(p).size
+  //Ouch move the declaration of FilterNeighbours above below the following two to encounter a NPE
+  //not that nice.
+  val AliveNeighbours = FilterNeighbours(_ == Alive)
+  val DeadNeighbours = FilterNeighbours(_ == Dead)
 }
+
 sealed trait State
 case object Alive extends State
 case object Dead extends State
 
-
-
 object StateTransition extends  (State => NeighbourhoodState => State) {
   def apply(present: State) =  (ns : NeighbourhoodState) =>  present match {
     case Alive => AliveNeighbours(ns) match {
-      case x if x == 2 || x == 3 => Alive
+      case 2 | 3 => Alive
       case _ => Dead
     }
     case _ => AliveNeighbours(ns) match {
-      case x if x == 3 => Alive
+      case 3 => Alive
       case _ => Dead
     }
   }
